5b73b46525e826d41cbc13a30042b08c207856c4
Colin Raymond <ckraymond@colin.comp>
2019-02-26 17:36:11 -0500

    First commit

---
+++ scapy_scan.py
@@ -0,0 +1,13 @@
+# Scapy Scan
+# Author: Colin Raymond (colin.k.raymond@gmail.com)
+
+import scapy.all as Scapy
+import os
+
+class ScapyScan:
+
+    def __init__(self):
+        os.system('clear')
+        print 'Performing ScapyScan'
+
+new_scan = ScapyScan()
